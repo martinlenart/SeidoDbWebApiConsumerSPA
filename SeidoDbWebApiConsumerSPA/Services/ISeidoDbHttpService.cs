31d068a56aff46471a33207bdd2697ca6ad0a204
Martin Lenart <martin_lenart@icloud.com>
2022-04-07 08:51:37 +0200

    Add project files.

---
+++ SeidoDbWebApiConsumerSPA/Services/ISeidoDbHttpService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SeidoDbWebApiConsumerSPA.Models;
+
+namespace SeidoDbWebApiConsumerSPA.Services
+{
+    public interface ISeidoDbHttpService
+    {
+        Task<IEnumerable<ICustomer>> GetCustomersAsync();
+        Task<ICustomer> GetCustomerAsync(Guid custId);
+
+        Task<ICustomer> UpdateCustomerAsync(Customer cus);
+
+        Task<ICustomer> CreateCustomerAsync(Customer cus);
+        Task<ICustomer> DeleteCustomerAsync(Guid custId);
+    }
+}
